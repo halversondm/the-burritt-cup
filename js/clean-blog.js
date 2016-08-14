c94bbd956c31bfd2bbc12cc239266d10473ca630
Dan Halverson <daniel.m.halverson@gmail.com>
2016-08-13 19:58:25 -0500

    integrating more pictures

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -50,7 +50,26 @@ jQuery(document).ready(function($) {
             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: 'title'
         }
+    });
+
+    $('.location-gallery').each(function () {
+        $(this).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: 'title'
+            }
+        });
     });
 });
