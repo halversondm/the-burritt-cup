18a7be0febdd9f133c7a1d2769d8b28ab0d308f6
Dan Halverson <daniel.m.halverson@gmail.com>
2018-02-04 12:54:57 -0600

    move toward a more standard directory structure.

--- gulpfile.js
+++ gulpfile.js
@@ -23,10 +23,10 @@ gulp.task("default", ["less", "minify-css", "minify-js", "copy"]);
 
 // Less task to compile the less files and add the banner
 gulp.task("less", function () {
-    return gulp.src("less/clean-blog.less")
+    return gulp.src("src/less/clean-blog.less")
         .pipe(less())
         .pipe(header(banner, {pkg: pkg}))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -34,10 +34,10 @@ gulp.task("less", function () {
 
 // Minify CSS
 gulp.task("minify-css", function () {
-    return gulp.src("css/clean-blog.css")
+    return gulp.src("src/css/clean-blog.css")
         .pipe(cleanCSS({compatibility: "ie8"}))
         .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -45,11 +45,11 @@ gulp.task("minify-css", function () {
 
 // Minify JS
 gulp.task("minify-js", function () {
-    return gulp.src("js/clean-blog.js")
+    return gulp.src("src/js/clean-blog.js")
         .pipe(uglify())
         .pipe(header(banner, {pkg: pkg}))
         .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest("js"))
+        .pipe(gulp.dest("src/js"))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -58,13 +58,13 @@ gulp.task("minify-js", function () {
 // Copy Bootstrap core files from node_modules to vendor directory
 gulp.task("bootstrap", function () {
     return gulp.src(["node_modules/bootstrap/dist/**/*", "!**/npm.js", "!**/bootstrap-theme.*", "!**/*.map"])
-        .pipe(gulp.dest("vendor/bootstrap"))
+        .pipe(gulp.dest("src/vendor/bootstrap"))
 });
 
 // Copy jQuery core files from node_modules to vendor directory
 gulp.task("jquery", function () {
     return gulp.src(["node_modules/jquery/dist/jquery.js", "node_modules/jquery/dist/jquery.min.js"])
-        .pipe(gulp.dest("vendor/jquery"))
+        .pipe(gulp.dest("src/vendor/jquery"))
 });
 
 // Copy Font Awesome core files from node_modules to vendor directory
@@ -77,15 +77,15 @@ gulp.task("fontawesome", function () {
         "!node_modules/font-awesome/*.md",
         "!node_modules/font-awesome/*.json"
     ])
-        .pipe(gulp.dest("vendor/font-awesome"))
+        .pipe(gulp.dest("src/vendor/font-awesome"))
 });
 
 // Copy Magnific Popup files from node_modules to vendor directory
 gulp.task("magnificpopup", function () {
     return gulp.src([
-        "node_modules/magnific-popup/**"
+        "node_modules/magnific-popup/dist/**"
     ])
-        .pipe(gulp.dest("vendor/magnific-popup"))
+        .pipe(gulp.dest("src/vendor/magnific-popup"))
 });
 
 // Copy all third party dependencies from node_modules to vendor directory
@@ -95,17 +95,35 @@ gulp.task("copy", ["bootstrap", "jquery", "fontawesome", "magnificpopup"]);
 gulp.task("browserSync", function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./src/"
+        },
+    })
+});
+
+// build process and build testing
+
+gulp.task("create-dist", function () {
+    gulp.src(["src/css/clean-blog.min.css"]).pipe(gulp.dest("dist/css/"));
+    gulp.src(["src/img/**"]).pipe(gulp.dest("dist/img"));
+    gulp.src(["src/js/clean-blog.min.js"]).pipe(gulp.dest("dist/js"));
+    gulp.src(["src/vendor/**"]).pipe(gulp.dest("dist/vendor"));
+    return gulp.src(["src/*.html"]).pipe(gulp.dest("dist"));
+});
+
+gulp.task("run-dist", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
         },
     })
 });
 
 // Watch Task that compiles LESS and watches for HTML or JS changes and reloads with browserSync
 gulp.task("dev", ["browserSync", "copy", "less", "minify-css", "minify-js"], function () {
-    gulp.watch("less/*.less", ["less"]);
-    gulp.watch("css/*.css", ["minify-css"]);
-    gulp.watch("js/*.js", ["minify-js"]);
+    gulp.watch("src/less/*.less", ["less"]);
+    gulp.watch("src/css/*.css", ["minify-css"]);
+    gulp.watch("src/js/*.js", ["minify-js"]);
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch("*.html", browserSync.reload);
-    gulp.watch("js/**/*.js", browserSync.reload);
+    gulp.watch("src/*.html", browserSync.reload);
+    gulp.watch("src/js/**/*.js", browserSync.reload);
 });
