088ba006cd210bffcec921a9283f76e2780ff6dd
Dan Halverson <daniel.m.halverson@gmail.com>
2018-03-25 22:17:37 -0500

    updating package dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const pkg = require("./package.json");
 const banner = ["/*!\n",
     " * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n",
     " * Copyright 2013-" + (new Date()).getFullYear(), " <%= pkg.author %>\n",
-    " * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n",
+    " * Licensed under <%= pkg.license %>\n",
     " */\n",
     ""
 ].join("");
