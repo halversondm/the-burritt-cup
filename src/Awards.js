82bb988fc4c15b228b09a25494c5bb5af5c753a6
Dan Halverson <daniel.m.halverson@gmail.com>
2019-03-16 13:13:30 -0500

    adding in unit tests

--- src/Awards.js
+++ src/Awards.js
@@ -4,27 +4,27 @@ class Awards extends Component {
 
     render() {
         return (
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
+<div className="container">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
             <h1>Awards</h1>
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <img class="img-responsive" src="./img/full/the_cup.jpg" alt="The Burritt Cup"/>
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <img className="img-responsive" src="./img/full/the_cup.jpg" alt="The Burritt Cup"/>
+                <h2 className="post-title">
                     The Burritt Cup
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     In all it's glory
                 </h4>
-                <p class="post-meta">This Cup is the goal of the weekend. The bragging rights for the next year go to
+                <p className="post-meta">This Cup is the goal of the weekend. The bragging rights for the next year go to
                     the winners of the Cup. Teams are chosen at random and vary from year to year.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
@@ -59,19 +59,19 @@ class Awards extends Component {
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <img class="img-responsive" src="./img/full/low_round.jpg" alt="Low Round"/>
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <img className="img-responsive" src="./img/full/low_round.jpg" alt="Low Round"/>
+                <h2 className="post-title">
                     Low Round
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     To the player, who shoots like a Tour player.
                 </h4>
-                <p class="post-meta">This award goes to the player who cards the lowest round out of three on the weekend.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">This award goes to the player who cards the lowest round out of three on the weekend.</p>
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
@@ -110,19 +110,19 @@ class Awards extends Component {
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <img class="img-responsive" src="./img/full/most_pars.jpg" alt="Most Pars"/>
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <img className="img-responsive" src="./img/full/most_pars.jpg" alt="Most Pars"/>
+                <h2 className="post-title">
                     Most Pars
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     To the player, who's working towards the Tour Card.
                 </h4>
-                <p class="post-meta">This award goes to the player with the most pars over three rounds of golf.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">This award goes to the player with the most pars over three rounds of golf.</p>
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
@@ -165,19 +165,19 @@ class Awards extends Component {
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <img class="img-responsive" src="./img/full/most_bogies.jpg" alt="Most Bogies"/>
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <img className="img-responsive" src="./img/full/most_bogies.jpg" alt="Most Bogies"/>
+                <h2 className="post-title">
                     Most Bogies
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     To the player, who's trailing the money on the Web.com Tour.
                 </h4>
-                <p class="post-meta">This award goes to the player with the most bogies over three rounds of golf.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">This award goes to the player with the most bogies over three rounds of golf.</p>
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
@@ -220,19 +220,19 @@ class Awards extends Component {
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <img class="img-responsive" src="./img/full/high_round.jpg" alt="High Round"/>
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <img className="img-responsive" src="./img/full/high_round.jpg" alt="High Round"/>
+                <h2 className="post-title">
                     High Round
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     To the player who's hacking away at the range, but sucking on the course.
                 </h4>
-                <p class="post-meta">This award goes to the player with the highest round over three rounds of golf.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">This award goes to the player with the highest round over three rounds of golf.</p>
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
@@ -271,18 +271,18 @@ class Awards extends Component {
         </div>
     </div>
     <hr/>
-    <div class="row justify-content-center">
-        <div class="col-lg-8 col-md-10">
-            <div class="post-preview">
-                <h2 class="post-title">
+    <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+            <div className="post-preview">
+                <h2 className="post-title">
                     Most Hazards
                 </h2>
-                <h4 class="post-subtitle">
+                <h4 className="post-subtitle">
                     To the player, who's working on his aim, but is just not there yet.
                 </h4>
-                <p class="post-meta">This award goes to the player with the most number of hazards hit over three rounds of golf.</p>
-                <p class="post-meta">Past Winners:
-                <table class="table table-striped table-responsive">
+                <p className="post-meta">This award goes to the player with the most number of hazards hit over three rounds of golf.</p>
+                <p className="post-meta">Past Winners:
+                <table className="table table-striped table-responsive">
                     <thead>
                     <tr>
                         <th>Year</th>
