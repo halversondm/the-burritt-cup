516872f38e2072f12edbc59bcacb676e537cac70
Dan Halverson <daniel.m.halverson@gmail.com>
2019-03-17 15:20:16 -0500

    completed moving locations into json

--- src/Post.js
+++ src/Post.js
@@ -15,19 +15,30 @@ class Post extends Component {
                             <h2 className="post-title">{this.props.title}</h2>
                             <h3 className="post-subtitle">{this.props.subtitle}</h3>
                             <p className="post-meta">{this.props.metatitle}</p>
+                            {this.props.summary !== "" ? <p className="post-meta"><u>Summary</u><br/>{this.props.summary}</p> : null}
+                            {this.props.awards.length > 0 ?
+                                <div className="post-preview">
+                                    <u className="post-meta">Awards</u>
+                                    <ul className="post-meta">
+                                        {this.props.awards.map((award, i) => { return (<li key={i}>{award}</li>) })}
+                                    </ul>
+                                </div>
+                                : null
+                            }
+                            {this.props.images.length > 0 ? <p className="post-meta"><u>Photo Gallery</u></p> : null}
                             <div className="location-gallery">
                                 {this.props.images.map((image, i) => {
-                                    return (<a href={image.location}
-                                        title={image.title} key={i}>
-                                        <img className="head" src={image.location} alt={image.alt} />
-                                    </a>)
+                                    return (
+                                        <a href={image.location} title={image.title} key={i}>
+                                            <img className="head" src={image.location} alt={image.alt} />
+                                        </a>
+                                    );
                                 })}
                             </div>
                         </div>
                     </Col>
                 </Row>
             </div>
-
         );
     }
 }
@@ -36,14 +47,18 @@ Post.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     metatitle: PropTypes.string,
-    images: PropTypes.array
+    images: PropTypes.array,
+    awards: PropTypes.array,
+    summary: PropTypes.string
 };
 
 Post.defaultProps = {
     title: "",
     subtitle: "",
     metatitle: "",
-    images: [{location: "", title: "", alt: ""}]
+    images: [],
+    awards: [],
+    summary: ""
 };
 
 export default Post;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import "font-awesome/css/font-awesome.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as $ from 'jquery';
+import 'magnific-popup';
+import 'magnific-popup/dist/magnific-popup.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -14,3 +17,26 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+$(document).ready(function($) {
+    if (typeof $().magnificPopup === 'function') {
+        // Initialize and Configure Magnific Popup Lightbox Plugin
+        $('.location-gallery').each(function () {
+            $(this).magnificPopup({
+                delegate: 'a',
+                type: 'image',
+                tLoading: 'Loading image #%curr%...',
+                mainClass: 'mfp-img-mobile',
+                gallery: {
+                    enabled: true,
+                    navigateByImgClick: true,
+                    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+                },
+                image: {
+                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                    titleSrc: 'title'
+                }
+            });
+        });
+    }
+});
